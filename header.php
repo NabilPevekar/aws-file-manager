17873f24216b333062b7feefc7420507e1255b69
NABIL DILDAR PEVEKAR <144530739+NabilPevekar@users.noreply.github.com>
2023-09-15 22:44:35 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>AWS File Manager</title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Include your custom CSS styles -->
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">AWS File Manager</a>
+    </nav>
+    <div class="container mt-4">
